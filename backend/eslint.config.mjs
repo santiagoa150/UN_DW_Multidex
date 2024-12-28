ce66d2778d85de8fa60beda74a33fa8ebcb35621
Santiago Álvarez Muñoz <santiagoa150@gmail.com>
2024-12-27 22:00:53 -0500

    feat: eslint configuration

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,16 @@
+import eslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default [
+    ...tsEslint.config(eslint.configs.recommended, ...tsEslint.configs.recommended, prettier, {
+        rules: {
+            'linebreak-style': ['error', 'unix'],
+            'max-depth': ['error', 4],
+            'object-curly-spacing': ['error', 'always'],
+            'prefer-template': ['error'],
+            quotes: ['error', 'single', { avoidEscape: true }],
+            semi: ['error', 'always'],
+        },
+    }),
+];
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 import * as process from 'node:process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap().then(() => {
-  console.log('Here');
+    console.log('Here');
 });
