83582949faa96a655e5ffe4e853b056bd2333424
Santiago Álvarez Muñoz <santiagoa150@gmail.com>
2024-12-27 22:25:50 -0500

    feat: swagger configuration

--- backend/src/config/main.ts
+++ backend/src/config/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import * as process from 'node:process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 /**
  * Bootstraps the API Gateway applications.
@@ -11,10 +12,18 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     // TODO: Configure exception filter and logger interceptor!
     app.enableCors();
+    app.setGlobalPrefix(process.env.APP_REST_PREFIX);
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Multidex App')
+        .setDescription('API Rest for Multidex App')
+        .addBearerAuth()
+        .build();
+    SwaggerModule.setup(process.env.SWAGGER_PREFIX, app, SwaggerModule.createDocument(app, swaggerConfig));
     await app.listen(process.env.APP_PORT);
     return app.get(Logger);
 }
 
 bootstrap().then((logger: Logger) => {
     logger.log(`Server listening on port :: ${process.env.APP_PORT}`);
+    logger.log(`Swagger listening on :: /${process.env.SWAGGER_PREFIX}`);
 });
