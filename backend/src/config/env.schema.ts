3c80c651984848915fb9845031c606b00b4cd667
Santiago Álvarez Muñoz <santiagoa150@gmail.com>
2025-01-07 23:41:30 -0500

    feat: database conecction with sequalize

--- backend/src/config/env.schema.ts
+++ backend/src/config/env.schema.ts
@@ -4,7 +4,13 @@ export const EnvSchema: Joi.ObjectSchema = Joi.object({
     APP_PORT: Joi.number().port(),
     APP_REST_PREFIX: Joi.string(),
     SWAGGER_PREFIX: Joi.string(),
+    POSTGRES_HOST: Joi.string(),
+    POSTGRES_PORT: Joi.number().port(),
     POSTGRES_USER: Joi.string(),
+    POSTGRES_PASSWORD: Joi.string(),
+    POSTGRES_DB: Joi.string(),
+    POSTGRES_SCHEMA: Joi.string(),
+    POSTGRES_SHOW_LOGS: Joi.number().valid(0, 1),
 })
     .options({ presence: 'required' })
     .required();
--- backend/src/config/modules/shared.module.ts
+++ backend/src/config/modules/shared.module.ts
@@ -1,6 +1,8 @@
 import { Global, Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EnvSchema } from '../env.schema';
+import { SequelizeModule } from '@nestjs/sequelize';
+import * as process from 'node:process';
 
 /**
  * Shared Configuration Module.
@@ -13,6 +15,18 @@ import { EnvSchema } from '../env.schema';
             isGlobal: true,
             validationSchema: EnvSchema,
         }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            schema: process.env.POSTGRES_SCHEMA,
+            autoLoadModels: true,
+            synchronize: true,
+            logging: Number(process.env.POSTGRES_SHOW_LOGS) ? (sql: string) => { new Logger().debug(sql); } : false,
+        })
     ],
     providers: [Logger, ConfigModule],
 })
